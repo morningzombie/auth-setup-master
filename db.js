44ce2c4db9f70d7e2359a87dc7eeaf23cac87032
morningzombie <56514455+morningzombie@users.noreply.github.com>
2020-03-16 15:45:39 -0400

    Add files via upload

--- db.js
+++ db.js
@@ -1,10 +1,14 @@
-const { Client } = require('pg');
-const bcrypt = require('bcrypt');
-const client = new Client(process.env.DATABASE_URL || 'postgres://localhost/acme_auth_db');
+const { Client } = require("pg");
+const bcrypt = require("bcrypt");
+const jwt = require("jwt-simple");
+
+const client = new Client(
+  process.env.DATABASE_URL || "postgres://localhost/acme_auth_db"
+);
 
 client.connect();
 
-const sync = async()=> {
+const sync = async () => {
   const SQL = `
   CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
   DROP TABLE IF EXISTS users;
@@ -16,60 +20,78 @@ const sync = async()=> {
   );
   `;
   await client.query(SQL);
+
   const [lucy, moe] = await Promise.all([
-    createUser({ username: 'moe', password: 'MOE'}),
-    createUser({ username: 'lucy', password: 'LUCY'}),
-    createUser({ username: 'curly', password: 'CURLY'})
+    createUser({ username: "moe", password: "MOE" }),
+    createUser({ username: "lucy", password: "LUCY" }),
+    createUser({ username: "curly", password: "CURLY" })
   ]);
+
   // console.log(await readUsers());
 
-  await authenticate({username:'lucy', password: 'LUCY'});
-  await authenticate({username:'moe', password: 'MOE'});
+  const token = await authenticate({ username: "lucy", password: "LUCY" });
 
+  const user = await findUserFromToken(token);
+  console.log(user);
+  //await authenticate({username:'moe', password: 'MOE'});
 };
 
-const authenticate = async({username, password})=>{
-const user=(await client.query('SELECT * from users WHERE username=$1',[username])).rows[0];
-await compare({ plain: password, hashed: user.password});
+const findUserFromToken = async (token) => {
+  const id = jwt.decode(token, process.env.JWT).id;
+  //TODO remove password from user
+  return (await client.query("SELECT * FROM users WHERE id=$1", [id])).rows[0];
 };
 
-const compare = async ({plain, hashed})=>{
-  return new Promise((resolve, reject)=>{
+const authenticate = async ({ username, password }) => {
+  const user = (
+    await client.query("SELECT * from users WHERE username=$1", [username])).rows[0];
+  await compare({ plain: password, hashed: user.password });
+  return jwt.encode({ id: user.id }, process.env.JWT);
+};
+
+const compare = async ({ plain, hashed }) => {
+  return new Promise((resolve, reject) => {
     bcrypt.compare(plain, hashed, (err, result) => {
-      if (err){
+      if (err) {
         return reject(err);
       }
-      if(result===true){
+      if (result === true) {
         return resolve();
       }
-      reject(Error('bad credentials'));
-    })
+      reject(Error("bad credentials"));
+    });
   });
 };
 
-const readUsers = async()=>{
+const readUsers = async () => {
   //TODO remove password
-  return (await client.query('SELECT * from users')).rows;
-}
+  return (await client.query("SELECT * from users")).rows;
+};
 
-const createUser = async({ username, password })=> {
+const createUser = async ({ username, password }) => {
   const hashed = await hash(password);
-  return (await client.query('INSERT INTO users(username, password) values ($1, $2) returning *', [ username, hashed])).rows[0];
+  return (
+    await client.query(
+      "INSERT INTO users(username, password) values ($1, $2) returning *",
+      [username, hashed]
+    )
+  ).rows[0];
 };
 
 //TODO
-const hash = (plain)=> {
-  return new Promise((resolve, reject)=>{
-    bcrypt.hash(plain, 10, (err, hashed)=>{
-      if(err){
+const hash = plain => {
+  return new Promise((resolve, reject) => {
+    bcrypt.hash(plain, 10, (err, hashed) => {
+      if (err) {
         return reject(err);
       }
       resolve(hashed);
-    })
+    });
   });
 };
 
-
 module.exports = {
-  sync
+  sync,
+  authenticate,
+  findUserFromToken
 };
--- server.js
+++ server.js
@@ -8,7 +8,38 @@ const db = require('./db');
 app.use('/dist', express.static(path.join(__dirname, 'dist')));
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
+app.use((req, res, next)=>{
+  if(req.headers.authentication){
+    return next();
+  }
+  db.findUserFromToken(req.headers.authentication)
+  .then(user=>{
+    req.user= user;
+    next();
+  })
+  .catch(next);
+});
+
+const isLoggedIn = (req, res, next)=> {
+  if(!req.user){
+    const err = Error('not authenticated');
+    err.status = 401;
+    return next(err);
+  }
+  next();
+};
+
 app.get('/', (req, res, next)=> res.sendFile(path.join(__dirname, 'index.html')));
+
+app.post('/api/auth', (req, res, next)=>{
+  db.authentication(req.body)
+  .then(token => res.send({token}))
+  .catch(next);
+});
+
+app.get('/api/auth', isLoggedIn, (req, res, next)=>{
+  res.send(req.user);
+  });
 
 app.use((err, req, res, next)=> {
   res.status(err.status || 500).send({ message: err.message});
