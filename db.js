9d9e820ddb29c0ff7e0b150776d0d6744f95b4e0
morningzombie <56514455+morningzombie@users.noreply.github.com>
2020-03-16 12:19:48 -0400

    Add files via upload

---
+++ db.js
@@ -0,0 +1,75 @@
+const { Client } = require('pg');
+const bcrypt = require('bcrypt');
+const client = new Client(process.env.DATABASE_URL || 'postgres://localhost/acme_auth_db');
+
+client.connect();
+
+const sync = async()=> {
+  const SQL = `
+  CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+  DROP TABLE IF EXISTS users;
+  CREATE TABLE users(
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    username VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR NOT NULL,
+    CHECK (char_length(username) > 0)
+  );
+  `;
+  await client.query(SQL);
+  const [lucy, moe] = await Promise.all([
+    createUser({ username: 'moe', password: 'MOE'}),
+    createUser({ username: 'lucy', password: 'LUCY'}),
+    createUser({ username: 'curly', password: 'CURLY'})
+  ]);
+  // console.log(await readUsers());
+
+  await authenticate({username:'lucy', password: 'LUCY'});
+  await authenticate({username:'moe', password: 'MOE'});
+
+};
+
+const authenticate = async({username, password})=>{
+const user=(await client.query('SELECT * from users WHERE username=$1',[username])).rows[0];
+await compare({ plain: password, hashed: user.password});
+};
+
+const compare = async ({plain, hashed})=>{
+  return new Promise((resolve, reject)=>{
+    bcrypt.compare(plain, hashed, (err, result) => {
+      if (err){
+        return reject(err);
+      }
+      if(result===true){
+        return resolve();
+      }
+      reject(Error('bad credentials'));
+    })
+  });
+};
+
+const readUsers = async()=>{
+  //TODO remove password
+  return (await client.query('SELECT * from users')).rows;
+}
+
+const createUser = async({ username, password })=> {
+  const hashed = await hash(password);
+  return (await client.query('INSERT INTO users(username, password) values ($1, $2) returning *', [ username, hashed])).rows[0];
+};
+
+//TODO
+const hash = (plain)=> {
+  return new Promise((resolve, reject)=>{
+    bcrypt.hash(plain, 10, (err, hashed)=>{
+      if(err){
+        return reject(err);
+      }
+      resolve(hashed);
+    })
+  });
+};
+
+
+module.exports = {
+  sync
+};
